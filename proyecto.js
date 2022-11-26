2bc421442452abf41df321d297988ecc36ee752b
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-25 23:07:59 -0400

    Finalizando funciones del proyecto

--- proyecto.js
+++ proyecto.js
@@ -2,11 +2,9 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, //Letras, numeros, guion y guion bajo. De 4 a 16 digitos.
-    nombre: /^[a-zA-ZÀ-Ý\s]{2,40}$/, //Letras y espacios, pueden llevar acentos.
-    password: /^.{4,15}$/, //De 4 a 12 digitos.
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    telefono: /^\d{7,14}$/ //De 7 a 14 numeros.
+    nombre: /^[a-zA-ZÀ-Ý\s]{3,40}$/, //Letras y espacios, pueden llevar acentos.
+    apellido: /^[a-zA-ZÀ-Ý\s]{3,40}$/, //Letras y espacios, pueden llevar acentos.
+    telefono: /^\d{11}$/ //11 numeros.
 }
 
 
@@ -14,26 +12,17 @@ const validarFormulario = (e) => {
     
     switch (e.target.name) {
         
-        case 'usuario':
-            validarCampo(expresiones.usuario, e.target, 'usuario');
-            
-            break;
         case 'nombre':
+            console.log('Hola');
             validarCampo(expresiones.nombre, e.target, 'nombre');
             
             break;
-        case 'password':
-            validarCampo(expresiones.password, e.target, 'password');
-            validarPassword2();
-            break;
-        case 'password2':
-            validarPassword2 ();            
+
+        case 'apellido':
+            validarCampo(expresiones.nombre, e.target, 'apellido');
             
             break;
-        case 'correo':
-            validarCampo(expresiones.correo, e.target, 'correo');
-            
-            break;
+
         case 'telefono':
             validarCampo(expresiones.telefono, e.target, 'telefono');
             
@@ -43,9 +32,117 @@ const validarFormulario = (e) => {
     
 }
 
+const validarCampo = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-check');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-xmark');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos [campo] = true;
+        
+    } else {
+        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} i`).classList.add('fa-circle-xmark');
+        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-circle-check');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos [campo] = false;
+    }
+}
+
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario); //Ocurre un evento al presionar una tecla dentro del input ("tecla levantada").
     input.addEventListener('blur', validarFormulario); //Ocurre un evento al presionar fuera del input.
 
+});
+
+
+var selectedRow = null;
+//Mostrar alertas.
+function showAlert(message, className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const main = document.querySelector('.main');
+    container.insertBefore(div, main);
+
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+}
+
+//Borrar campos
+function clearFields() {
+    document.querySelector('#nombre').value = '';
+    document.querySelector('#apellido').value = '';
+    document.querySelector('#telefono').value = '';    
+}
+
+
+//Agregar datos
+document.querySelector('#formulario').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Obtener valores del formulario
+    const nombre = document.querySelector('#nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const telefono = document.querySelector('#telefono').value;
+
+    //Validar
+    if (nombre == '' || apellido == '' || telefono == '') {
+        showAlert('Por favor llena todos los campos.', 'danger');        
+    } else {
+        if (selectedRow == null) {
+            const list = document.querySelector('#contact-list');
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${nombre}</td>
+            <td>${apellido}</td>
+            <td>${telefono}</td>
+            <td>
+            <a href="#" class="btn btn-warning bt-sm edit">Editar</a>
+            <a href="#" class="btn btn-danger bt-sm delete">Borrar</a>
+            </td>            
+            `;
+            list.appendChild(row);
+            selectedRow = null;
+            showAlert('Nuevo contacto agregado.', "success");            
+        } else {
+            selectedRow.children[0].textContent = nombre;
+            selectedRow.children[1].textContent = apellido;
+            selectedRow.children[2].textContent = telefono;
+            selectedRow = null;
+            showAlert('Informacion de contacto actualizada.', 'info');
+        }
+
+        clearFields();
+
+
+    }
+});
+
+
+//Editar datos
+document.querySelector('#contact-list').addEventListener('click', (e) => {
+    target = e.target;
+    if(target.classList.contains('edit')){
+        selectedRow = target.parentElement.parentElement;
+        document.querySelector('#nombre').value = selectedRow.children[0].textContent;
+        document.querySelector('#apellido').value = selectedRow.children[1].textContent;
+        document.querySelector('#telefono').value = selectedRow.children[2].textContent;
+    }
+})
+
+//Delete data
+document.querySelector('#contact-list').addEventListener('click', (e) =>{
+    target = e.target;
+
+    alert('Esta a punto de eliminar el contacto.')
+    if (target.classList.contains('delete')) {
+        target.parentElement.parentElement.remove();
+        showAlert('Contacto borrado.', 'danger');        
+    }
 });
